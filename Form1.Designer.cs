9d3c4722fec07ef460b6fce17d04b003237aa58f
PiPeSiTo <andresfobando@unicauca.edu.co>
2025-09-01 20:56:20 -0500

    .

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -30,26 +30,26 @@
         {
             this.tcGestionRecolectores = new System.Windows.Forms.TabControl();
             this.tpRegistrarRecolector = new System.Windows.Forms.TabPage();
-            this.tabPage2 = new System.Windows.Forms.TabPage();
-            this.gbRegistrarRecolector = new System.Windows.Forms.GroupBox();
-            this.lblCedula = new System.Windows.Forms.Label();
-            this.lblNombre = new System.Windows.Forms.Label();
-            this.lblTelefono = new System.Windows.Forms.Label();
-            this.txtCedula = new System.Windows.Forms.TextBox();
-            this.txtNombre = new System.Windows.Forms.TextBox();
-            this.txtTelefono = new System.Windows.Forms.TextBox();
-            this.btnRegistrar = new System.Windows.Forms.Button();
-            this.btnLimpiarCampos = new System.Windows.Forms.Button();
             this.gbListaRecolectores = new System.Windows.Forms.GroupBox();
-            this.dgvRecolectores = new System.Windows.Forms.DataGridView();
-            this.btnDarBaja = new System.Windows.Forms.Button();
-            this.btnReactivar = new System.Windows.Forms.Button();
             this.btnActualizarLista = new System.Windows.Forms.Button();
+            this.btnReactivar = new System.Windows.Forms.Button();
+            this.btnDarBaja = new System.Windows.Forms.Button();
+            this.dgvRecolectores = new System.Windows.Forms.DataGridView();
+            this.gbRegistrarRecolector = new System.Windows.Forms.GroupBox();
+            this.btnLimpiarCampos = new System.Windows.Forms.Button();
+            this.btnRegistrar = new System.Windows.Forms.Button();
+            this.txtTelefono = new System.Windows.Forms.TextBox();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtCedula = new System.Windows.Forms.TextBox();
+            this.lblTelefono = new System.Windows.Forms.Label();
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.lblCedula = new System.Windows.Forms.Label();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
             this.tcGestionRecolectores.SuspendLayout();
             this.tpRegistrarRecolector.SuspendLayout();
-            this.gbRegistrarRecolector.SuspendLayout();
             this.gbListaRecolectores.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dgvRecolectores)).BeginInit();
+            this.gbRegistrarRecolector.SuspendLayout();
             this.SuspendLayout();
             // 
             // tcGestionRecolectores
@@ -74,15 +74,55 @@
             this.tpRegistrarRecolector.Text = "Gestión Recolector";
             this.tpRegistrarRecolector.UseVisualStyleBackColor = true;
             // 
-            // tabPage2
+            // gbListaRecolectores
             // 
-            this.tabPage2.Location = new System.Drawing.Point(4, 25);
-            this.tabPage2.Name = "tabPage2";
-            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(192, 71);
-            this.tabPage2.TabIndex = 1;
-            this.tabPage2.Text = "tabPage2";
-            this.tabPage2.UseVisualStyleBackColor = true;
+            this.gbListaRecolectores.Controls.Add(this.btnActualizarLista);
+            this.gbListaRecolectores.Controls.Add(this.btnReactivar);
+            this.gbListaRecolectores.Controls.Add(this.btnDarBaja);
+            this.gbListaRecolectores.Controls.Add(this.dgvRecolectores);
+            this.gbListaRecolectores.Location = new System.Drawing.Point(448, 41);
+            this.gbListaRecolectores.Name = "gbListaRecolectores";
+            this.gbListaRecolectores.Size = new System.Drawing.Size(389, 247);
+            this.gbListaRecolectores.TabIndex = 1;
+            this.gbListaRecolectores.TabStop = false;
+            this.gbListaRecolectores.Text = "Lista de Recolectores Registrados";
+            // 
+            // btnActualizarLista
+            // 
+            this.btnActualizarLista.Location = new System.Drawing.Point(251, 161);
+            this.btnActualizarLista.Name = "btnActualizarLista";
+            this.btnActualizarLista.Size = new System.Drawing.Size(115, 35);
+            this.btnActualizarLista.TabIndex = 3;
+            this.btnActualizarLista.Text = "Actualizar Lista";
+            this.btnActualizarLista.UseVisualStyleBackColor = true;
+            // 
+            // btnReactivar
+            // 
+            this.btnReactivar.Location = new System.Drawing.Point(149, 162);
+            this.btnReactivar.Name = "btnReactivar";
+            this.btnReactivar.Size = new System.Drawing.Size(85, 34);
+            this.btnReactivar.TabIndex = 2;
+            this.btnReactivar.Text = "Reactivar";
+            this.btnReactivar.UseVisualStyleBackColor = true;
+            // 
+            // btnDarBaja
+            // 
+            this.btnDarBaja.Location = new System.Drawing.Point(22, 163);
+            this.btnDarBaja.Name = "btnDarBaja";
+            this.btnDarBaja.Size = new System.Drawing.Size(111, 33);
+            this.btnDarBaja.TabIndex = 1;
+            this.btnDarBaja.Text = "Dar de Baja";
+            this.btnDarBaja.UseVisualStyleBackColor = true;
+            // 
+            // dgvRecolectores
+            // 
+            this.dgvRecolectores.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvRecolectores.Location = new System.Drawing.Point(37, 42);
+            this.dgvRecolectores.Name = "dgvRecolectores";
+            this.dgvRecolectores.RowHeadersWidth = 51;
+            this.dgvRecolectores.RowTemplate.Height = 24;
+            this.dgvRecolectores.Size = new System.Drawing.Size(314, 88);
+            this.dgvRecolectores.TabIndex = 0;
             // 
             // gbRegistrarRecolector
             // 
@@ -101,53 +141,14 @@
             this.gbRegistrarRecolector.TabStop = false;
             this.gbRegistrarRecolector.Text = "Registrar Nuevo Recolector";
             // 
-            // lblCedula
+            // btnLimpiarCampos
             // 
-            this.lblCedula.AutoSize = true;
-            this.lblCedula.Location = new System.Drawing.Point(42, 49);
-            this.lblCedula.Name = "lblCedula";
-            this.lblCedula.Size = new System.Drawing.Size(53, 16);
-            this.lblCedula.TabIndex = 0;
-            this.lblCedula.Text = "Cédula:";
-            // 
-            // lblNombre
-            // 
-            this.lblNombre.AutoSize = true;
-            this.lblNombre.Location = new System.Drawing.Point(42, 89);
-            this.lblNombre.Name = "lblNombre";
-            this.lblNombre.Size = new System.Drawing.Size(59, 16);
-            this.lblNombre.TabIndex = 1;
-            this.lblNombre.Text = "Nombre:";
-            // 
-            // lblTelefono
-            // 
-            this.lblTelefono.AutoSize = true;
-            this.lblTelefono.Location = new System.Drawing.Point(42, 130);
-            this.lblTelefono.Name = "lblTelefono";
-            this.lblTelefono.Size = new System.Drawing.Size(64, 16);
-            this.lblTelefono.TabIndex = 2;
-            this.lblTelefono.Text = "Teléfono:";
-            // 
-            // txtCedula
-            // 
-            this.txtCedula.Location = new System.Drawing.Point(101, 46);
-            this.txtCedula.Name = "txtCedula";
-            this.txtCedula.Size = new System.Drawing.Size(100, 22);
-            this.txtCedula.TabIndex = 3;
-            // 
-            // txtNombre
-            // 
-            this.txtNombre.Location = new System.Drawing.Point(107, 86);
-            this.txtNombre.Name = "txtNombre";
-            this.txtNombre.Size = new System.Drawing.Size(138, 22);
-            this.txtNombre.TabIndex = 4;
-            // 
-            // txtTelefono
-            // 
-            this.txtTelefono.Location = new System.Drawing.Point(112, 127);
-            this.txtTelefono.Name = "txtTelefono";
-            this.txtTelefono.Size = new System.Drawing.Size(100, 22);
-            this.txtTelefono.TabIndex = 5;
+            this.btnLimpiarCampos.Location = new System.Drawing.Point(191, 189);
+            this.btnLimpiarCampos.Name = "btnLimpiarCampos";
+            this.btnLimpiarCampos.Size = new System.Drawing.Size(128, 33);
+            this.btnLimpiarCampos.TabIndex = 7;
+            this.btnLimpiarCampos.Text = "Limpiar Campos";
+            this.btnLimpiarCampos.UseVisualStyleBackColor = true;
             // 
             // btnRegistrar
             // 
@@ -158,64 +159,63 @@
             this.btnRegistrar.Text = "Registrar";
             this.btnRegistrar.UseVisualStyleBackColor = true;
             // 
-            // btnLimpiarCampos
+            // txtTelefono
             // 
-            this.btnLimpiarCampos.Location = new System.Drawing.Point(192, 189);
-            this.btnLimpiarCampos.Name = "btnLimpiarCampos";
-            this.btnLimpiarCampos.Size = new System.Drawing.Size(128, 33);
-            this.btnLimpiarCampos.TabIndex = 7;
-            this.btnLimpiarCampos.Text = "Limpiar Campos";
-            this.btnLimpiarCampos.UseVisualStyleBackColor = true;
+            this.txtTelefono.Location = new System.Drawing.Point(112, 127);
+            this.txtTelefono.Name = "txtTelefono";
+            this.txtTelefono.Size = new System.Drawing.Size(100, 22);
+            this.txtTelefono.TabIndex = 5;
             // 
-            // gbListaRecolectores
+            // txtNombre
             // 
-            this.gbListaRecolectores.Controls.Add(this.btnActualizarLista);
-            this.gbListaRecolectores.Controls.Add(this.btnReactivar);
-            this.gbListaRecolectores.Controls.Add(this.btnDarBaja);
-            this.gbListaRecolectores.Controls.Add(this.dgvRecolectores);
-            this.gbListaRecolectores.Location = new System.Drawing.Point(448, 41);
-            this.gbListaRecolectores.Name = "gbListaRecolectores";
-            this.gbListaRecolectores.Size = new System.Drawing.Size(389, 247);
-            this.gbListaRecolectores.TabIndex = 1;
-            this.gbListaRecolectores.TabStop = false;
-            this.gbListaRecolectores.Text = "Lista de Recolectores Registrados";
+            this.txtNombre.Location = new System.Drawing.Point(107, 86);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(138, 22);
+            this.txtNombre.TabIndex = 4;
             // 
-            // dgvRecolectores
+            // txtCedula
             // 
-            this.dgvRecolectores.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvRecolectores.Location = new System.Drawing.Point(37, 42);
-            this.dgvRecolectores.Name = "dgvRecolectores";
-            this.dgvRecolectores.RowHeadersWidth = 51;
-            this.dgvRecolectores.RowTemplate.Height = 24;
-            this.dgvRecolectores.Size = new System.Drawing.Size(314, 88);
-            this.dgvRecolectores.TabIndex = 0;
+            this.txtCedula.Location = new System.Drawing.Point(101, 46);
+            this.txtCedula.Name = "txtCedula";
+            this.txtCedula.Size = new System.Drawing.Size(100, 22);
+            this.txtCedula.TabIndex = 3;
             // 
-            // btnDarBaja
+            // lblTelefono
             // 
-            this.btnDarBaja.Location = new System.Drawing.Point(22, 163);
-            this.btnDarBaja.Name = "btnDarBaja";
-            this.btnDarBaja.Size = new System.Drawing.Size(111, 33);
-            this.btnDarBaja.TabIndex = 1;
-            this.btnDarBaja.Text = "Dar de Baja";
-            this.btnDarBaja.UseVisualStyleBackColor = true;
+            this.lblTelefono.AutoSize = true;
+            this.lblTelefono.Location = new System.Drawing.Point(42, 130);
+            this.lblTelefono.Name = "lblTelefono";
+            this.lblTelefono.Size = new System.Drawing.Size(64, 16);
+            this.lblTelefono.TabIndex = 2;
+            this.lblTelefono.Text = "Teléfono:";
             // 
-            // btnReactivar
+            // lblNombre
             // 
-            this.btnReactivar.Location = new System.Drawing.Point(149, 162);
-            this.btnReactivar.Name = "btnReactivar";
-            this.btnReactivar.Size = new System.Drawing.Size(85, 34);
-            this.btnReactivar.TabIndex = 2;
-            this.btnReactivar.Text = "Reactivar";
-            this.btnReactivar.UseVisualStyleBackColor = true;
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Location = new System.Drawing.Point(42, 89);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(59, 16);
+            this.lblNombre.TabIndex = 1;
+            this.lblNombre.Text = "Nombre:";
             // 
-            // btnActualizarLista
+            // lblCedula
             // 
-            this.btnActualizarLista.Location = new System.Drawing.Point(251, 161);
-            this.btnActualizarLista.Name = "btnActualizarLista";
-            this.btnActualizarLista.Size = new System.Drawing.Size(115, 35);
-            this.btnActualizarLista.TabIndex = 3;
-            this.btnActualizarLista.Text = "Actualizar Lista";
-            this.btnActualizarLista.UseVisualStyleBackColor = true;
+            this.lblCedula.AutoSize = true;
+            this.lblCedula.Location = new System.Drawing.Point(42, 49);
+            this.lblCedula.Name = "lblCedula";
+            this.lblCedula.Size = new System.Drawing.Size(53, 16);
+            this.lblCedula.TabIndex = 0;
+            this.lblCedula.Text = "Cédula:";
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 25);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(879, 331);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
             // 
             // frmGestionRecolectores
             // 
@@ -227,10 +227,10 @@
             this.Text = "Sistema de Gestión de Pagos - Caficultora";
             this.tcGestionRecolectores.ResumeLayout(false);
             this.tpRegistrarRecolector.ResumeLayout(false);
-            this.gbRegistrarRecolector.ResumeLayout(false);
-            this.gbRegistrarRecolector.PerformLayout();
             this.gbListaRecolectores.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.dgvRecolectores)).EndInit();
+            this.gbRegistrarRecolector.ResumeLayout(false);
+            this.gbRegistrarRecolector.PerformLayout();
             this.ResumeLayout(false);
 
         }
