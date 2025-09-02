8bd824592dd7a15287db6bda4fe04d9b1f0ce98e
juanmoscoso3 <juanmoscoso@unicauca.edu.co>
2025-09-01 21:59:12 -0500

    Avance interfaz clase  harvest, se implemento interfaz en form1.cs y lógica en form1.cs

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,26 +28,230 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.groupBoxDatos = new System.Windows.Forms.GroupBox();
+            this.dtpStartDate = new System.Windows.Forms.DateTimePicker();
+            this.dtpEndDate = new System.Windows.Forms.DateTimePicker();
+            this.lblStartDate = new System.Windows.Forms.Label();
+            this.lblPrice = new System.Windows.Forms.Label();
+            this.lblEndDate = new System.Windows.Forms.Label();
+            this.txtPrice = new System.Windows.Forms.TextBox();
+            this.lblLocation = new System.Windows.Forms.Label();
+            this.txtLocation = new System.Windows.Forms.TextBox();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.btnVerColectas = new System.Windows.Forms.Button();
+            this.dgvCosechas = new System.Windows.Forms.DataGridView();
+            this.btnEditar = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.btnExportar = new System.Windows.Forms.Button();
+            this.btnCerrar = new System.Windows.Forms.Button();
+            this.lblId = new System.Windows.Forms.Label();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.groupBoxDatos.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCosechas)).BeginInit();
             this.SuspendLayout();
             // 
-            // label1
+            // lblTitulo
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(321, 99);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(69, 16);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "CAFEPAY";
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.Location = new System.Drawing.Point(200, 20);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(182, 13);
+            this.lblTitulo.TabIndex = 1;
+            this.lblTitulo.Text = "GESTIÓN DE COSECHAS DE CAFÉ";
+            // 
+            // groupBoxDatos
+            // 
+            this.groupBoxDatos.Controls.Add(this.txtId);
+            this.groupBoxDatos.Controls.Add(this.dtpStartDate);
+            this.groupBoxDatos.Controls.Add(this.lblId);
+            this.groupBoxDatos.Controls.Add(this.dtpEndDate);
+            this.groupBoxDatos.Controls.Add(this.lblStartDate);
+            this.groupBoxDatos.Controls.Add(this.lblPrice);
+            this.groupBoxDatos.Controls.Add(this.lblEndDate);
+            this.groupBoxDatos.Controls.Add(this.txtPrice);
+            this.groupBoxDatos.Controls.Add(this.lblLocation);
+            this.groupBoxDatos.Controls.Add(this.txtLocation);
+            this.groupBoxDatos.Location = new System.Drawing.Point(32, 46);
+            this.groupBoxDatos.Name = "groupBoxDatos";
+            this.groupBoxDatos.Size = new System.Drawing.Size(350, 250);
+            this.groupBoxDatos.TabIndex = 2;
+            this.groupBoxDatos.TabStop = false;
+            this.groupBoxDatos.Text = "DATOS DE LA COSECHA";
+            // 
+            // dtpStartDate
+            // 
+            this.dtpStartDate.Location = new System.Drawing.Point(133, 75);
+            this.dtpStartDate.Name = "dtpStartDate";
+            this.dtpStartDate.Size = new System.Drawing.Size(150, 20);
+            this.dtpStartDate.TabIndex = 15;
+            // 
+            // dtpEndDate
+            // 
+            this.dtpEndDate.Location = new System.Drawing.Point(133, 108);
+            this.dtpEndDate.Name = "dtpEndDate";
+            this.dtpEndDate.Size = new System.Drawing.Size(150, 20);
+            this.dtpEndDate.TabIndex = 17;
+            // 
+            // lblStartDate
+            // 
+            this.lblStartDate.AutoSize = true;
+            this.lblStartDate.Location = new System.Drawing.Point(23, 81);
+            this.lblStartDate.Name = "lblStartDate";
+            this.lblStartDate.Size = new System.Drawing.Size(68, 13);
+            this.lblStartDate.TabIndex = 14;
+            this.lblStartDate.Text = "Fecha Inicio:";
+            // 
+            // lblPrice
+            // 
+            this.lblPrice.AutoSize = true;
+            this.lblPrice.Location = new System.Drawing.Point(23, 153);
+            this.lblPrice.Name = "lblPrice";
+            this.lblPrice.Size = new System.Drawing.Size(62, 13);
+            this.lblPrice.TabIndex = 18;
+            this.lblPrice.Text = "\tPrecio/Kilo:";
+            // 
+            // lblEndDate
+            // 
+            this.lblEndDate.AutoSize = true;
+            this.lblEndDate.Location = new System.Drawing.Point(28, 114);
+            this.lblEndDate.Name = "lblEndDate";
+            this.lblEndDate.Size = new System.Drawing.Size(57, 13);
+            this.lblEndDate.TabIndex = 16;
+            this.lblEndDate.Text = "Fecha Fin:";
+            // 
+            // txtPrice
+            // 
+            this.txtPrice.Location = new System.Drawing.Point(133, 153);
+            this.txtPrice.Name = "txtPrice";
+            this.txtPrice.Size = new System.Drawing.Size(150, 20);
+            this.txtPrice.TabIndex = 19;
+            // 
+            // lblLocation
+            // 
+            this.lblLocation.AutoSize = true;
+            this.lblLocation.Location = new System.Drawing.Point(21, 201);
+            this.lblLocation.Name = "lblLocation";
+            this.lblLocation.Size = new System.Drawing.Size(58, 13);
+            this.lblLocation.TabIndex = 20;
+            this.lblLocation.Text = "Ubicación:";
+            // 
+            // txtLocation
+            // 
+            this.txtLocation.Location = new System.Drawing.Point(133, 198);
+            this.txtLocation.Name = "txtLocation";
+            this.txtLocation.Size = new System.Drawing.Size(200, 20);
+            this.txtLocation.TabIndex = 21;
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(450, 100);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(200, 40);
+            this.btnAgregar.TabIndex = 3;
+            this.btnAgregar.Text = "AGREGAR COSECHA";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.Location = new System.Drawing.Point(450, 160);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(200, 40);
+            this.btnLimpiar.TabIndex = 4;
+            this.btnLimpiar.Text = "LIMPIAR CAMPOS";
+            this.btnLimpiar.UseVisualStyleBackColor = true;
+            // 
+            // btnVerColectas
+            // 
+            this.btnVerColectas.Location = new System.Drawing.Point(450, 220);
+            this.btnVerColectas.Name = "btnVerColectas";
+            this.btnVerColectas.Size = new System.Drawing.Size(200, 40);
+            this.btnVerColectas.TabIndex = 5;
+            this.btnVerColectas.Text = "VER COLECTAS";
+            this.btnVerColectas.UseVisualStyleBackColor = true;
+            // 
+            // dgvCosechas
+            // 
+            this.dgvCosechas.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvCosechas.Location = new System.Drawing.Point(56, 302);
+            this.dgvCosechas.Name = "dgvCosechas";
+            this.dgvCosechas.Size = new System.Drawing.Size(700, 200);
+            this.dgvCosechas.TabIndex = 6;
+            // 
+            // btnEditar
+            // 
+            this.btnEditar.Location = new System.Drawing.Point(56, 518);
+            this.btnEditar.Name = "btnEditar";
+            this.btnEditar.Size = new System.Drawing.Size(100, 30);
+            this.btnEditar.TabIndex = 7;
+            this.btnEditar.Text = "EDITAR";
+            this.btnEditar.UseVisualStyleBackColor = true;
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Location = new System.Drawing.Point(176, 518);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(100, 30);
+            this.btnEliminar.TabIndex = 8;
+            this.btnEliminar.Text = "ELIMINAR";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            // 
+            // btnExportar
+            // 
+            this.btnExportar.Location = new System.Drawing.Point(580, 518);
+            this.btnExportar.Name = "btnExportar";
+            this.btnExportar.Size = new System.Drawing.Size(100, 30);
+            this.btnExportar.TabIndex = 9;
+            this.btnExportar.Text = "EXPORTAR";
+            this.btnExportar.UseVisualStyleBackColor = true;
+            // 
+            // btnCerrar
+            // 
+            this.btnCerrar.Location = new System.Drawing.Point(702, 11);
+            this.btnCerrar.Name = "btnCerrar";
+            this.btnCerrar.Size = new System.Drawing.Size(30, 30);
+            this.btnCerrar.TabIndex = 11;
+            this.btnCerrar.Text = "X";
+            this.btnCerrar.UseVisualStyleBackColor = true;
+            // 
+            // lblId
+            // 
+            this.lblId.AutoSize = true;
+            this.lblId.Location = new System.Drawing.Point(25, 47);
+            this.lblId.Name = "lblId";
+            this.lblId.Size = new System.Drawing.Size(66, 13);
+            this.lblId.TabIndex = 12;
+            this.lblId.Text = "ID Cosecha:";
+            // 
+            // txtId
+            // 
+            this.txtId.Location = new System.Drawing.Point(133, 40);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(111, 20);
+            this.txtId.TabIndex = 13;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.label1);
+            this.ClientSize = new System.Drawing.Size(784, 611);
+            this.Controls.Add(this.btnCerrar);
+            this.Controls.Add(this.btnExportar);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btnEditar);
+            this.Controls.Add(this.dgvCosechas);
+            this.Controls.Add(this.btnVerColectas);
+            this.Controls.Add(this.btnLimpiar);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.groupBoxDatos);
+            this.Controls.Add(this.lblTitulo);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Sistema de Gestión de Cosechas";
+            this.groupBoxDatos.ResumeLayout(false);
+            this.groupBoxDatos.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCosechas)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -55,7 +259,26 @@
 
         #endregion
 
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.GroupBox groupBoxDatos;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.Button btnLimpiar;
+        private System.Windows.Forms.Button btnVerColectas;
+        private System.Windows.Forms.DataGridView dgvCosechas;
+        private System.Windows.Forms.Button btnEditar;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Button btnExportar;
+        private System.Windows.Forms.Button btnCerrar;
+        private System.Windows.Forms.Label lblId;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.Label lblStartDate;
+        private System.Windows.Forms.DateTimePicker dtpStartDate;
+        private System.Windows.Forms.Label lblEndDate;
+        private System.Windows.Forms.DateTimePicker dtpEndDate;
+        private System.Windows.Forms.Label lblPrice;
+        private System.Windows.Forms.TextBox txtPrice;
+        private System.Windows.Forms.Label lblLocation;
+        private System.Windows.Forms.TextBox txtLocation;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -12,9 +12,154 @@ namespace CAFEPAY
 {
     public partial class Form1 : Form
     {
+        private List<Harvest> cosechas = new List<Harvest>();
+        private int currentId = 1;
+
         public Form1()
         {
             InitializeComponent();
+            // Configuración inicial
+            txtId.Text = currentId.ToString();
+            dtpStartDate.Value = DateTime.Today;
+            dtpEndDate.Value = DateTime.Today;
         }
+
+        // EVENTO para btnAgregar (doble click en el botón en el diseñador)
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Validar campos obligatorios
+                if (string.IsNullOrWhiteSpace(txtPrice.Text) ||
+                    string.IsNullOrWhiteSpace(txtLocation.Text))
+                {
+                    MessageBox.Show("Por favor complete todos los campos", "Advertencia",
+                        MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+
+                // Crear nueva cosecha
+                Harvest nuevaCosecha = new Harvest
+                {
+                    attIdHarvest = currentId,
+                    attStarDate = dtpStartDate.Value,
+                    attEndDate = dtpEndDate.Value,
+                    price_per_kilo = int.Parse(txtPrice.Text),
+                    attLocation = txtLocation.Text,
+                    attCollections = new List<Collects>()
+                };
+
+                cosechas.Add(nuevaCosecha);
+                currentId++;
+
+                // Actualizar interfaz
+                txtId.Text = currentId.ToString();
+                ActualizarDataGridView();
+
+                MessageBox.Show("Cosecha agregada exitosamente!", "Éxito",
+                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                LimpiarCampos();
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Por favor ingrese un precio válido", "Error",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error: {ex.Message}", "Error",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        // EVENTO para btnLimpiar
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            LimpiarCampos();
+        }
+
+        // EVENTO para btnVerColectas
+        private void btnVerColectas_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Funcionalidad de ver colectas en desarrollo", "Información",
+                MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        // EVENTO para btnCerrar
+        private void btnCerrar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        // EVENTO para btnEditar
+        private void btnEditar_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Funcionalidad de editar en desarrollo", "Información",
+                MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        // EVENTO para btnEliminar
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Funcionalidad de eliminar en desarrollo", "Información",
+                MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        // EVENTO para btnExportar
+        private void btnExportar_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Funcionalidad de exportar en desarrollo", "Información",
+                MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void LimpiarCampos()
+        {
+            dtpStartDate.Value = DateTime.Today;
+            dtpEndDate.Value = DateTime.Today;
+            txtPrice.Text = "";
+            txtLocation.Text = "";
+            txtPrice.Focus(); // Poner foco en el primer campo editable
+        }
+
+        private void ActualizarDataGridView()
+        {
+            dgvCosechas.DataSource = null;
+            dgvCosechas.DataSource = cosechas;
+
+            // Opcional: Formatear columnas
+            if (dgvCosechas.Columns.Count > 0)
+            {
+                dgvCosechas.Columns["attIdHarvest"].HeaderText = "ID";
+                dgvCosechas.Columns["attStarDate"].HeaderText = "Fecha Inicio";
+                dgvCosechas.Columns["attEndDate"].HeaderText = "Fecha Fin";
+                dgvCosechas.Columns["price_per_kilo"].HeaderText = "Precio/Kg";
+                dgvCosechas.Columns["attLocation"].HeaderText = "Ubicación";
+            }
+        }
+    }
+
+    // CLASES (fuera de la clase Form1)
+    public class Harvest
+    {
+        public int attIdHarvest { get; set; }
+        public DateTime attStarDate { get; set; }
+        public DateTime attEndDate { get; set; }
+        public int price_per_kilo { get; set; }
+        public string attLocation { get; set; }
+        public List<Collects> attCollections { get; set; }
+
+        public Harvest()
+        {
+            attCollections = new List<Collects>();
+        }
+    }
+
+    public class Collects
+    {
+        public int CollectId { get; set; }
+        public decimal Weight { get; set; }
+        public DateTime CollectDate { get; set; }
+        public string CollectorName { get; set; }
     }
 }
