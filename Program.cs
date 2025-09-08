4b7e60e7d1bff3e92d00636cace06c35fcf1bf12
SantiagoGutierrez11 <0011saga@gmail.com>
2025-09-08 09:33:01 -0500

    Create Backup branch

--- Program.cs
+++ Program.cs
@@ -15,6 +15,7 @@ namespace CAFEPAY
         [STAThread]
         static void Main()
         {
+            //
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new ViewHarvest());
