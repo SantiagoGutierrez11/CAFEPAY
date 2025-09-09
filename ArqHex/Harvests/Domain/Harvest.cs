319ce0b18cc9cecd0a2666a328ef347be545825a
Jhoiner <jpuentesfigueroagmail.com>
2025-09-09 14:19:46 -0500

    Agrego arquitectura hexagonal Harvest

---
+++ ArqHex/Harvests/Domain/Harvest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CAFEPAY.ArqHex.Harvests.Domain
+{
+    internal class Harvest
+    {
+        public HarvestId Id;
+        public HarvestStartDate StartDate;
+        public HarvestEndDate EndDate;
+        public HarvestPricePerKilo PricePerKilo;
+        public HarvestLocation Location;
+
+        public Harvest(HarvestId _harvestId, HarvestStartDate _harvestStartDate, HarvestEndDate _harvestEndDate,
+            HarvestPricePerKilo _harvestPricePerKilo, HarvestLocation _harvestLocation)
+        {
+            this.Id = _harvestId;
+            this.StartDate = _harvestStartDate;
+            this.EndDate = _harvestEndDate;
+            this.PricePerKilo = _harvestPricePerKilo;
+            this.Location = _harvestLocation;
+        }
+        public void recordHarvest()
+        {
+
+        }
+    }
+}
---
+++ ArqHex/Harvests/Domain/HarvestPricePerKilo.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CAFEPAY.ArqHex.Harvests.Domain
+{
+    internal class HarvestPricePerKilo
+    {
+        public decimal pricePerKiloValue;
+
+        public HarvestPricePerKilo(decimal _pricePerKiloValue)
+        {
+            this.pricePerKiloValue = _pricePerKiloValue;
+            ValidateFormat();
+        }
+
+        private void ValidateFormat()
+        {
+            if (this.pricePerKiloValue <= 0)
+            {
+                throw new ArgumentException("Harvest price per kilo must be greater than zero");
+            }
+
+            if (this.pricePerKiloValue > 999999.99m)
+            {
+                throw new ArgumentException("Harvest price per kilo is unrealistically high");
+            }
+        }
+
+        public decimal GetValue()
+        {
+            return this.pricePerKiloValue;
+        }
+    }
+}
