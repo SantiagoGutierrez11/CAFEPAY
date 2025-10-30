50d5666292943a6ec649fef8be4252e8e4ecd069
SantiagoGutierrez11 <0011saga@gmail.com>
2025-10-30 15:59:30 -0500

    Incorporacion de vistas de collector

--- Program.cs
+++ Program.cs
@@ -19,7 +19,7 @@ namespace CAFEPAY
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new ViewHarvest());
+            Application.Run(new ViewCollector());
             /*
                         ViewCollector viewCollector = new ViewCollector();
             viewCollector.Show();
