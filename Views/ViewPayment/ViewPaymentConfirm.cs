47d6bf7aeec836cdb92ccbe723acf4014fa6c4dc
SantiagoGutierrez11 <0011saga@gmail.com>
2025-11-18 14:15:45 -0500

    Incluyo funcionalidad pago y consultar pago

--- Views/ViewPayment/ViewPaymentConfirm.cs
+++ Views/ViewPayment/ViewPaymentConfirm.cs
@@ -125,8 +125,8 @@ namespace CAFEPAY.Views.ViewPayment
         {
 
         }
-
-        // 
+        //
+        //
         private void button1_Click(object sender, EventArgs e) //buton confirm
         {
             try
