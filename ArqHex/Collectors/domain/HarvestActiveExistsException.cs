2c69763545119564d0c34bf02fbfd263055ccfc0
Andrés Obando <andresfobando@unicauca.edu.co>
2025-11-27 00:25:52 -0500

    Revert "Integración con la rama Santiago"

--- ArqHex/Collectors/domain/HarvestActiveExistsException.cs
+++ ArqHex/Collectors/domain/HarvestActiveExistsException.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CAFEPAY.ArqHex.Harvests.domain
+namespace CAFEPAY.ArqHex.Collectors.domain
 {
     public class HarvestActiveExistsException : Exception
     {
