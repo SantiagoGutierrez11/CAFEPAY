b33b6d6df7a859e95cca84a0c0aeb7e556ab4393
PiPeSiTo21 <andresfobando@unicauca.edu.co>
2025-12-08 21:10:10 -0500

    Diseño de ViewMenuPayment

--- Views/ViewPayment/ViewMenuPayment.Designer.cs
+++ Views/ViewPayment/ViewMenuPayment.Designer.cs
@@ -28,59 +28,9 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.btnConsult = new System.Windows.Forms.Button();
-            this.btnPayment = new System.Windows.Forms.Button();
-            this.btnBack = new System.Windows.Forms.Button();
-            this.SuspendLayout();
-            // 
-            // btnConsult
-            // 
-            this.btnConsult.Location = new System.Drawing.Point(288, 87);
-            this.btnConsult.Name = "btnConsult";
-            this.btnConsult.Size = new System.Drawing.Size(207, 53);
-            this.btnConsult.TabIndex = 1;
-            this.btnConsult.Text = "Gestionar Pagos";
-            this.btnConsult.UseVisualStyleBackColor = true;
-            this.btnConsult.Click += new System.EventHandler(this.btnConsult_Click);
-            // 
-            // btnPayment
-            // 
-            this.btnPayment.Location = new System.Drawing.Point(23, 87);
-            this.btnPayment.Name = "btnPayment";
-            this.btnPayment.Size = new System.Drawing.Size(207, 53);
-            this.btnPayment.TabIndex = 2;
-            this.btnPayment.Text = "Pagar";
-            this.btnPayment.UseVisualStyleBackColor = true;
-            this.btnPayment.Click += new System.EventHandler(this.btnPayment_Click);
-            // 
-            // btnBack
-            // 
-            this.btnBack.Location = new System.Drawing.Point(247, 208);
-            this.btnBack.Name = "btnBack";
-            this.btnBack.Size = new System.Drawing.Size(109, 42);
-            this.btnBack.TabIndex = 3;
-            this.btnBack.Text = "Regresar";
-            this.btnBack.UseVisualStyleBackColor = true;
-            this.btnBack.Click += new System.EventHandler(this.btnBack_Click);
-            // 
-            // ViewMenuPayment
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(618, 280);
-            this.Controls.Add(this.btnBack);
-            this.Controls.Add(this.btnPayment);
-            this.Controls.Add(this.btnConsult);
-            this.Name = "ViewMenuPayment";
-            this.Text = "ViewMainPayment";
-            this.ResumeLayout(false);
-
+            // DEJA ESTO VACÍO - porque creamos controles dinámicamente
         }
 
         #endregion
-
-        private System.Windows.Forms.Button btnConsult;
-        private System.Windows.Forms.Button btnPayment;
-        private System.Windows.Forms.Button btnBack;
     }
 }
