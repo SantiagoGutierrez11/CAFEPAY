b8b53fa6a7818510e8e8b228c2f73cbf6356219e
juanmoscoso3 <juanmoscoso@unicauca.edu.co>
2025-12-11 04:18:06 -0500

    DIseño ViewPaymentDelete realizado

--- Views/ViewPayment/ViewPaymentDelete.Designer.cs
+++ Views/ViewPayment/ViewPaymentDelete.Designer.cs
@@ -29,11 +29,8 @@
         private void InitializeComponent()
         {
             this.textBoxPaymentAmount = new System.Windows.Forms.TextBox();
-            this.label11 = new System.Windows.Forms.Label();
             this.textBoxPaymentDate = new System.Windows.Forms.TextBox();
-            this.label6 = new System.Windows.Forms.Label();
             this.textBoxIdPayment = new System.Windows.Forms.TextBox();
-            this.label10 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.textBoxStatus = new System.Windows.Forms.TextBox();
@@ -59,59 +56,32 @@
             // textBoxPaymentAmount
             // 
             this.textBoxPaymentAmount.Enabled = false;
-            this.textBoxPaymentAmount.Location = new System.Drawing.Point(688, 100);
+            this.textBoxPaymentAmount.Location = new System.Drawing.Point(677, 97);
             this.textBoxPaymentAmount.Name = "textBoxPaymentAmount";
             this.textBoxPaymentAmount.Size = new System.Drawing.Size(137, 20);
             this.textBoxPaymentAmount.TabIndex = 72;
             // 
-            // label11
-            // 
-            this.label11.AutoSize = true;
-            this.label11.Location = new System.Drawing.Point(622, 104);
-            this.label11.Name = "label11";
-            this.label11.Size = new System.Drawing.Size(60, 13);
-            this.label11.TabIndex = 71;
-            this.label11.Text = "Monto total";
-            // 
             // textBoxPaymentDate
             // 
             this.textBoxPaymentDate.Enabled = false;
-            this.textBoxPaymentDate.Location = new System.Drawing.Point(688, 65);
+            this.textBoxPaymentDate.Location = new System.Drawing.Point(677, 65);
             this.textBoxPaymentDate.Name = "textBoxPaymentDate";
             this.textBoxPaymentDate.Size = new System.Drawing.Size(137, 20);
             this.textBoxPaymentDate.TabIndex = 70;
             // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(643, 69);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(37, 13);
-            this.label6.TabIndex = 69;
-            this.label6.Text = "Fecha";
-            // 
             // textBoxIdPayment
             // 
             this.textBoxIdPayment.Enabled = false;
-            this.textBoxIdPayment.Location = new System.Drawing.Point(688, 39);
+            this.textBoxIdPayment.Location = new System.Drawing.Point(677, 39);
             this.textBoxIdPayment.Name = "textBoxIdPayment";
             this.textBoxIdPayment.Size = new System.Drawing.Size(137, 20);
             this.textBoxIdPayment.TabIndex = 68;
-            // 
-            // label10
-            // 
-            this.label10.AutoSize = true;
-            this.label10.Location = new System.Drawing.Point(666, 42);
-            this.label10.Name = "label10";
-            this.label10.Size = new System.Drawing.Size(16, 13);
-            this.label10.TabIndex = 67;
-            this.label10.Text = "Id";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(726, 12);
+            this.label5.Location = new System.Drawing.Point(709, 12);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(58, 24);
             this.label5.TabIndex = 66;
@@ -130,7 +100,7 @@
             // textBoxStatus
             // 
             this.textBoxStatus.Enabled = false;
-            this.textBoxStatus.Location = new System.Drawing.Point(175, 101);
+            this.textBoxStatus.Location = new System.Drawing.Point(172, 101);
             this.textBoxStatus.Name = "textBoxStatus";
             this.textBoxStatus.Size = new System.Drawing.Size(137, 20);
             this.textBoxStatus.TabIndex = 64;
@@ -148,7 +118,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(130, 104);
+            this.label2.Location = new System.Drawing.Point(119, 100);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(40, 13);
             this.label2.TabIndex = 62;
@@ -157,7 +127,7 @@
             // textBoxPhone
             // 
             this.textBoxPhone.Enabled = false;
-            this.textBoxPhone.Location = new System.Drawing.Point(379, 76);
+            this.textBoxPhone.Location = new System.Drawing.Point(382, 76);
             this.textBoxPhone.Name = "textBoxPhone";
             this.textBoxPhone.Size = new System.Drawing.Size(137, 20);
             this.textBoxPhone.TabIndex = 61;
@@ -165,7 +135,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(334, 80);
+            this.label1.Location = new System.Drawing.Point(324, 76);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(39, 13);
             this.label1.TabIndex = 60;
@@ -174,7 +144,7 @@
             // textBoxWorkerName
             // 
             this.textBoxWorkerName.Enabled = false;
-            this.textBoxWorkerName.Location = new System.Drawing.Point(175, 73);
+            this.textBoxWorkerName.Location = new System.Drawing.Point(172, 73);
             this.textBoxWorkerName.Name = "textBoxWorkerName";
             this.textBoxWorkerName.Size = new System.Drawing.Size(137, 20);
             this.textBoxWorkerName.TabIndex = 59;
@@ -182,7 +152,7 @@
             // textBoxIdWorker
             // 
             this.textBoxIdWorker.Enabled = false;
-            this.textBoxIdWorker.Location = new System.Drawing.Point(434, 50);
+            this.textBoxIdWorker.Location = new System.Drawing.Point(443, 47);
             this.textBoxIdWorker.Name = "textBoxIdWorker";
             this.textBoxIdWorker.Size = new System.Drawing.Size(137, 20);
             this.textBoxIdWorker.TabIndex = 58;
@@ -190,7 +160,7 @@
             // textBoxWorkerCode
             // 
             this.textBoxWorkerCode.Enabled = false;
-            this.textBoxWorkerCode.Location = new System.Drawing.Point(175, 47);
+            this.textBoxWorkerCode.Location = new System.Drawing.Point(172, 46);
             this.textBoxWorkerCode.Name = "textBoxWorkerCode";
             this.textBoxWorkerCode.Size = new System.Drawing.Size(137, 20);
             this.textBoxWorkerCode.TabIndex = 57;
@@ -198,7 +168,7 @@
             // label9
             // 
             this.label9.AutoSize = true;
-            this.label9.Location = new System.Drawing.Point(122, 76);
+            this.label9.Location = new System.Drawing.Point(112, 72);
             this.label9.Name = "label9";
             this.label9.Size = new System.Drawing.Size(47, 13);
             this.label9.TabIndex = 56;
@@ -207,7 +177,7 @@
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(334, 53);
+            this.label8.Location = new System.Drawing.Point(324, 46);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(94, 13);
             this.label8.TabIndex = 55;
@@ -216,7 +186,7 @@
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(64, 50);
+            this.label7.Location = new System.Drawing.Point(41, 46);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(105, 13);
             this.label7.TabIndex = 54;
@@ -273,9 +243,9 @@
             this.label12.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label12.Location = new System.Drawing.Point(53, 478);
             this.label12.Name = "label12";
-            this.label12.Size = new System.Drawing.Size(353, 24);
+            this.label12.Size = new System.Drawing.Size(365, 24);
             this.label12.TabIndex = 75;
-            this.label12.Text = "Especifique la razon de la elimiacion";
+            this.label12.Text = "Especifique la razon de la eliminación";
             // 
             // textBoxReason
             // 
@@ -295,11 +265,8 @@
             this.Controls.Add(this.btnDeleteDetailPayment);
             this.Controls.Add(this.btnDeletPayment);
             this.Controls.Add(this.textBoxPaymentAmount);
-            this.Controls.Add(this.label11);
             this.Controls.Add(this.textBoxPaymentDate);
-            this.Controls.Add(this.label6);
             this.Controls.Add(this.textBoxIdPayment);
-            this.Controls.Add(this.label10);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.textBoxStatus);
@@ -327,11 +294,8 @@
         #endregion
 
         private System.Windows.Forms.TextBox textBoxPaymentAmount;
-        private System.Windows.Forms.Label label11;
         private System.Windows.Forms.TextBox textBoxPaymentDate;
-        private System.Windows.Forms.Label label6;
         private System.Windows.Forms.TextBox textBoxIdPayment;
-        private System.Windows.Forms.Label label10;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.TextBox textBoxStatus;
--- Views/ViewPayment/ViewPaymentDelete.cs
+++ Views/ViewPayment/ViewPaymentDelete.cs
@@ -5,32 +5,287 @@ using CAFEPAY.ArqHex.Share;
 using CAFEPAY.ArqHex.Share.DTO;
 using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace CAFEPAY.Views.ViewPayment
+
 {
     public partial class ViewPaymentDelete : Form
     {
+        // Colores profesionales (manteniendo los del original)
         private Color darkBlueColor = Color.FromArgb(13, 43, 97);
         private Color whiteColor = Color.White;
+        private Color redColor = Color.FromArgb(183, 32, 46);     // #B7202E
+        private Color darkGrayColor = Color.FromArgb(64, 64, 64);
+        private Color lightGrayColor = Color.FromArgb(240, 240, 240);
+
         private List<PaymentDetailDTO> paymentDetailDTOs;
         private CollectorDTO collectorDTO;
         private PaymentDTO payment;
+
         public ViewPaymentDelete(CollectorDTO _collector, PaymentDTO _payment, List<PaymentDetailDTO> _paymentDetailsDTOs)
         {
             InitializeComponent();
+            this.WindowState = FormWindowState.Maximized;
             this.paymentDetailDTOs = _paymentDetailsDTOs;
             this.collectorDTO = _collector;
             this.payment = _payment;
 
+            // Aplicar mejoras estéticas manteniendo el layout original
+            ApplyVisualImprovements();
+
             ConfigureDataGridView();
         }
+
+        private void ApplyVisualImprovements()
+        {
+            try
+            {
+                // 1. Estilo del formulario (manteniendo el original)
+                this.BackColor = whiteColor;
+                this.Font = new Font("Segoe UI", 9, FontStyle.Regular);
+                this.StartPosition = FormStartPosition.CenterScreen;
+                this.Text = "CAFICAUCA - Eliminar Pago";
+
+                // 2. Estilo de botones (mejorados)
+                StyleButtons();
+
+                // 3. Mejorar campos de texto (con la nueva fuente)
+                StyleTextBoxes();
+
+                // 4. Mejorar etiquetas (con la nueva fuente y colores)
+                StyleLabels();
+
+                // 5. Mejorar DataGridView (manteniendo la configuración original pero con mejoras)
+                StyleDataGridView();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error aplicando mejoras visuales: " + ex.Message);
+            }
+        }
+
+        private void StyleButtons()
+        {
+            // Botón "Eliminar Pago" (button1)
+            if (btnDeletPayment != null)
+            {
+                btnDeletPayment.BackColor = redColor;
+                btnDeletPayment.ForeColor = whiteColor;
+                btnDeletPayment.Font = new Font("Segoe UI Semibold", 10, FontStyle.Regular);
+                btnDeletPayment.FlatStyle = FlatStyle.Flat;
+                btnDeletPayment.FlatAppearance.BorderSize = 0;
+                btnDeletPayment.FlatAppearance.MouseOverBackColor = Color.FromArgb(163, 22, 36);
+                btnDeletPayment.Cursor = Cursors.Hand;
+                ApplyRoundedCorners(btnDeletPayment, 6);
+            }
+
+            // Botón "Eliminar Detalle" (btnDeleteDetailPayment)
+            if (btnDeleteDetailPayment != null)
+            {
+                btnDeleteDetailPayment.BackColor = redColor;
+                btnDeleteDetailPayment.ForeColor = whiteColor;
+                btnDeleteDetailPayment.Font = new Font("Segoe UI Semibold", 10, FontStyle.Regular);
+                btnDeleteDetailPayment.FlatStyle = FlatStyle.Flat;
+                btnDeleteDetailPayment.FlatAppearance.BorderSize = 0;
+                btnDeleteDetailPayment.FlatAppearance.MouseOverBackColor = Color.FromArgb(163, 22, 36);
+                btnDeleteDetailPayment.Cursor = Cursors.Hand;
+                ApplyRoundedCorners(btnDeleteDetailPayment, 6);
+            }
+
+            // Botón "Regresar" (btnBack)
+            if (btnBack != null)
+            {
+                btnBack.BackColor = darkGrayColor;
+                btnBack.ForeColor = whiteColor;
+                btnBack.Font = new Font("Segoe UI Semibold", 10, FontStyle.Regular);
+                btnBack.FlatStyle = FlatStyle.Flat;
+                btnBack.FlatAppearance.BorderSize = 0;
+                btnBack.FlatAppearance.MouseOverBackColor = Color.FromArgb(84, 84, 84);
+                btnBack.Cursor = Cursors.Hand;
+                ApplyRoundedCorners(btnBack, 6);
+            }
+        }
+
+        private void StyleTextBoxes()
+        {
+            // Campos de información del recolector (solo lectura) - CON NUEVA FUENTE
+            var collectorTextBoxes = new[] {
+                textBoxIdWorker, textBoxWorkerCode, textBoxWorkerName,
+                textBoxPhone, textBoxStatus
+            };
+
+            foreach (var textBox in collectorTextBoxes)
+            {
+                if (textBox != null)
+                {
+                    // Nueva fuente: Segoe UI Semibold para mejor legibilidad
+                    textBox.Font = new Font("Segoe UI Semibold", 9.5f, FontStyle.Regular);
+                    textBox.BorderStyle = BorderStyle.FixedSingle;
+                    textBox.BackColor = Color.FromArgb(250, 250, 250);
+                    textBox.ForeColor = Color.FromArgb(50, 50, 50);
+                    textBox.ReadOnly = true;
+                }
+            }
+
+            // Campos de información del pago - MISMA FUENTE que los del recolector
+            var paymentTextBoxes = new[] {
+                textBoxIdPayment, textBoxPaymentDate, textBoxPaymentAmount
+            };
+
+            foreach (var textBox in paymentTextBoxes)
+            {
+                if (textBox != null)
+                {
+                    // Misma fuente que los campos del recolector
+                    textBox.Font = new Font("Segoe UI Semibold", 9.5f, FontStyle.Regular);
+                    textBox.BorderStyle = BorderStyle.FixedSingle;
+                    textBox.BackColor = Color.FromArgb(250, 250, 250);
+                    textBox.ForeColor = Color.FromArgb(50, 50, 50);
+                    textBox.ReadOnly = true;
+                }
+            }
+
+            // Campo de razón (editable) - CON NUEVA FUENTE
+            if (textBoxReason != null)
+            {
+                textBoxReason.Font = new Font("Segoe UI", 10);
+                textBoxReason.BorderStyle = BorderStyle.FixedSingle;
+                textBoxReason.BackColor = Color.FromArgb(255, 245, 245);
+                textBoxReason.ForeColor = Color.FromArgb(64, 64, 64);
+            }
+        }
+
+        private void StyleLabels()
+        {
+            // ========== TÍTULOS PRINCIPALES ==========
+
+            // "Datos del recolector" (label4) - CON NUEVA FUENTE Y ESTILO
+            if (label4 != null)
+            {
+                label4.Font = new Font("Segoe UI", 12, FontStyle.Bold);
+                label4.ForeColor = darkBlueColor;
+            }
+
+            // "Pago Realizados" (label3) - MISMO ESTILO que "Datos del recolector"
+            if (label3 != null)
+            {
+                label3.Font = new Font("Segoe UI", 12, FontStyle.Bold);
+                label3.ForeColor = darkBlueColor;
+            }
+
+            // ========== LABELS DE CAMPOS DEL RECOLECTOR ==========
+
+            // Labels de campos del recolector (Código trabajador, Número de cédula, etc.)
+            var collectorFieldLabels = new[] { label7, label8, label9, label1, label2 };
+            foreach (var label in collectorFieldLabels)
+            {
+                if (label != null)
+                {
+                    // Nueva fuente para mejor legibilidad
+                    label.Font = new Font("Segoe UI Semibold", 10, FontStyle.Regular);
+                    label.ForeColor = darkBlueColor;
+                }
+            }
+
+            // ========== LABELS DE CAMPOS DE PAGO (crearlos si no existen) ==========
+
+            // Buscar o crear label para ID Pago
+            var lblIdPayment = this.Controls.Find("lblIdPayment", true).FirstOrDefault() as Label;
+            if (lblIdPayment == null && textBoxIdPayment != null)
+            {
+                lblIdPayment = new Label();
+                lblIdPayment.Text = "ID Pago:";
+                lblIdPayment.Name = "lblIdPayment";
+                lblIdPayment.Font = new Font("Segoe UI Semibold", 10, FontStyle.Regular);
+                lblIdPayment.ForeColor = darkBlueColor;
+                lblIdPayment.Location = textBoxIdPayment.Location;
+                lblIdPayment.Location = new Point(lblIdPayment.Location.X - 100, lblIdPayment.Location.Y);
+                lblIdPayment.AutoSize = true;
+                this.Controls.Add(lblIdPayment);
+            }
+            else if (lblIdPayment != null)
+            {
+                // Aplicar el mismo estilo que los labels del recolector
+                lblIdPayment.Font = new Font("Segoe UI Semibold", 10, FontStyle.Regular);
+                lblIdPayment.ForeColor = darkBlueColor;
+            }
+
+            // Buscar o crear label para Fecha
+            var lblPaymentDate = this.Controls.Find("lblPaymentDate", true).FirstOrDefault() as Label;
+            if (lblPaymentDate == null && textBoxPaymentDate != null)
+            {
+                lblPaymentDate = new Label();
+                lblPaymentDate.Text = "Fecha:";
+                lblPaymentDate.Name = "lblPaymentDate";
+                lblPaymentDate.Font = new Font("Segoe UI Semibold", 10, FontStyle.Regular);
+                lblPaymentDate.ForeColor = darkBlueColor;
+                lblPaymentDate.Location = textBoxPaymentDate.Location;
+                lblPaymentDate.Location = new Point(lblPaymentDate.Location.X - 100, lblPaymentDate.Location.Y);
+                lblPaymentDate.AutoSize = true;
+                this.Controls.Add(lblPaymentDate);
+            }
+            else if (lblPaymentDate != null)
+            {
+                // Aplicar el mismo estilo que los labels del recolector
+                lblPaymentDate.Font = new Font("Segoe UI Semibold", 10, FontStyle.Regular);
+                lblPaymentDate.ForeColor = darkBlueColor;
+            }
+
+            // Buscar o crear label para Monto Total
+            var lblPaymentAmount = this.Controls.Find("lblPaymentAmount", true).FirstOrDefault() as Label;
+            if (lblPaymentAmount == null && textBoxPaymentAmount != null)
+            {
+                lblPaymentAmount = new Label();
+                lblPaymentAmount.Text = "Monto Total:";
+                lblPaymentAmount.Name = "lblPaymentAmount";
+                lblPaymentAmount.Font = new Font("Segoe UI Semibold", 10, FontStyle.Regular);
+                lblPaymentAmount.ForeColor = darkBlueColor;
+                lblPaymentAmount.Location = textBoxPaymentAmount.Location;
+                lblPaymentAmount.Location = new Point(lblPaymentAmount.Location.X - 100, lblPaymentAmount.Location.Y);
+                lblPaymentAmount.AutoSize = true;
+                this.Controls.Add(lblPaymentAmount);
+            }
+            else if (lblPaymentAmount != null)
+            {
+                // Aplicar el mismo estilo que los labels del recolector
+                lblPaymentAmount.Font = new Font("Segoe UI Semibold", 10, FontStyle.Regular);
+                lblPaymentAmount.ForeColor = darkBlueColor;
+            }
+        }
+
+        private void StyleDataGridView()
+        {
+            // Mantener la configuración original pero mejorar la fuente
+            dgPaymentDetails.ColumnHeadersDefaultCellStyle.Font = new Font("Segoe UI Semibold", 10.5f, FontStyle.Regular);
+            dgPaymentDetails.DefaultCellStyle.Font = new Font("Segoe UI", 9.5f);
+        }
+
+        private void ApplyRoundedCorners(Control control, int radius)
+        {
+            try
+            {
+                GraphicsPath path = new GraphicsPath();
+                Rectangle rect = new Rectangle(0, 0, control.Width, control.Height);
+
+                path.AddArc(rect.X, rect.Y, radius, radius, 180, 90);
+                path.AddArc(rect.X + rect.Width - radius, rect.Y, radius, radius, 270, 90);
+                path.AddArc(rect.X + rect.Width - radius, rect.Y + rect.Height - radius, radius, radius, 0, 90);
+                path.AddArc(rect.X, rect.Y + rect.Height - radius, radius, radius, 90, 90);
+                path.CloseFigure();
+
+                control.Region = new Region(path);
+            }
+            catch
+            {
+                // Si falla, continuar sin esquinas redondeadas
+            }
+        }
+
+        // ========== MÉTODOS ORIGINALES (SIN CAMBIOS DE FUNCIONALIDAD) ==========
 
         private void ViewPaymentDelete_Load(object sender, EventArgs e)
         {
@@ -38,6 +293,7 @@ namespace CAFEPAY.Views.ViewPayment
             loadDataCollector();
             loadDataPayment();
         }
+
         public void loadDataCollector()
         {
             textBoxIdWorker.Text = collectorDTO.id.ToString();
@@ -46,15 +302,22 @@ namespace CAFEPAY.Views.ViewPayment
             textBoxPhone.Text = collectorDTO.phone;
             textBoxStatus.Text = collectorDTO.statusText;
         }
+
         public void loadDataPayment()
         {
             textBoxIdPayment.Text = payment.Id.ToString();
             textBoxPaymentDate.Text = payment.Date.ToString("dd/MM/yyyy");
-            textBoxPaymentAmount.Text = payment.TotalAmount.ToString("C2");
+
+            // Formato de moneda mejorado
+            var culture = new System.Globalization.CultureInfo("es-CO");
+            culture.NumberFormat.CurrencySymbol = "$";
+            culture.NumberFormat.CurrencyPositivePattern = 2;
+            textBoxPaymentAmount.Text = payment.TotalAmount.ToString("C2", culture);
         }
+
         private void ConfigureDataGridView()
         {
-            // Configuración visual del DataGridView
+            // Configuración visual del DataGridView (ORIGINAL)
             dgPaymentDetails.BorderStyle = BorderStyle.None;
             dgPaymentDetails.BackgroundColor = whiteColor;
             dgPaymentDetails.DefaultCellStyle.SelectionBackColor = Color.FromArgb(230, 240, 250);
@@ -69,15 +332,15 @@ namespace CAFEPAY.Views.ViewPayment
             dgPaymentDetails.MultiSelect = true;
             dgPaymentDetails.ReadOnly = true;
 
-            // Estilo de encabezados
+            // Estilo de encabezados (CON NUEVA FUENTE)
             dgPaymentDetails.ColumnHeadersDefaultCellStyle.BackColor = darkBlueColor;
             dgPaymentDetails.ColumnHeadersDefaultCellStyle.ForeColor = whiteColor;
-            dgPaymentDetails.ColumnHeadersDefaultCellStyle.Font = new Font("Segoe UI", 11, FontStyle.Bold);
+            dgPaymentDetails.ColumnHeadersDefaultCellStyle.Font = new Font("Segoe UI Semibold", 11, FontStyle.Regular);
             dgPaymentDetails.ColumnHeadersDefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
             dgPaymentDetails.ColumnHeadersDefaultCellStyle.Padding = new Padding(10, 0, 0, 0);
             dgPaymentDetails.ColumnHeadersHeight = 40;
 
-            // Estilo de celdas
+            // Estilo de celdas (CON NUEVA FUENTE)
             dgPaymentDetails.DefaultCellStyle.Font = new Font("Segoe UI", 10);
             dgPaymentDetails.DefaultCellStyle.BackColor = whiteColor;
             dgPaymentDetails.DefaultCellStyle.ForeColor = Color.FromArgb(60, 60, 60);
@@ -226,7 +489,7 @@ namespace CAFEPAY.Views.ViewPayment
 
             try
             {
-               foreach (var detail in paymentDetailDTOs)
+                foreach (var detail in paymentDetailDTOs)
                 {
                     AppServices.PaymentDetailServices.deleteByPaymentDetailId.execute(detail.Id, reason);
                 }
@@ -304,8 +567,8 @@ namespace CAFEPAY.Views.ViewPayment
                 {
                     if (row.DataBoundItem is PaymentDetailDTO paymentDetail)
                     {
-                       AppServices.PaymentDetailServices.deleteByPaymentDetailId.execute(paymentDetail.Id, reason);
-                          iDsPaymentDetail.Add(paymentDetail.Id.ToString());
+                        AppServices.PaymentDetailServices.deleteByPaymentDetailId.execute(paymentDetail.Id, reason);
+                        iDsPaymentDetail.Add(paymentDetail.Id.ToString());
                     }
                 }
                 string joinedIds = string.Join(",", iDsPaymentDetail);
@@ -321,7 +584,7 @@ namespace CAFEPAY.Views.ViewPayment
                     viewOwner.LoadPayments();
                     viewOwner.Show();
                 }
-                   
+
                 this.Close();
             }
             catch (Exception ex)
@@ -340,4 +603,3 @@ namespace CAFEPAY.Views.ViewPayment
         }
     }
 }
-
